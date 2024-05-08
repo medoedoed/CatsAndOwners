bfeed4454e3d6398c8c8397674971c649bce0ebd
medoedoed <medoedoded@yandex.ru>
2024-05-08 14:51:47 +0300

    cats jpa updated

--- CatsAndOwners/dao/src/main/java/ru/medoedoed/models/Cat.java
+++ CatsAndOwners/dao/src/main/java/ru/medoedoed/models/Cat.java
@@ -33,7 +33,7 @@ public class Cat implements JpaEntity {
   @ManyToOne
   private Owner owner;
 
-  @ManyToMany(cascade = CascadeType.ALL)
+  @ManyToMany()
   @JoinTable(
       name = "CatFriends",
       joinColumns = @JoinColumn(name = "FirstCatId"),
--- CatsAndOwners/services/build.gradle.kts
+++ CatsAndOwners/services/build.gradle.kts
@@ -15,18 +15,19 @@ dependencies {
     implementation(project(":dao"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
     implementation("io.jsonwebtoken:jjwt:0.2")
 
     testCompileOnly("org.projectlombok:lombok:1.18.30")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
     implementation("org.springframework.security:spring-security-web:6.2.3")
     implementation("org.springframework.security:spring-security-config:6.2.3")
     implementation( "org.springframework.boot:spring-boot-starter-validation:3.2.4")
+    implementation("org.springframework.security:spring-security-core:6.2.4")
 }
 
 tasks.test {
