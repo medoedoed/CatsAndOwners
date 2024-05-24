3c106fbf7986115db8646736c12c5c049914ae5f
medoedoed <medoedoded@yandex.ru>
2024-05-25 00:24:59 +0300

    Color Rabbit Config updated

--- catColorService/src/main/java/ru/medoedoed/rabbitmq/RabbitConfig.java
+++ catColorService/src/main/java/ru/medoedoed/rabbitmq/RabbitConfig.java
@@ -1,38 +1,64 @@
 package ru.medoedoed.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class RabbitConfig {
-  @Value("${rabbitmq.host}")
-  private String host;
-
-  @Value("${rabbitmq.port}")
-  private int port;
-
-  @Value("${rabbitmq.username}")
-  private String username;
-
-  @Value("${rabbitmq.password}")
-  private String password;
-
   @Bean
   public ConnectionFactory connectionFactory() {
-    CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-    connectionFactory.setHost(host);
-    connectionFactory.setPort(port);
-    connectionFactory.setUsername(username);
-    connectionFactory.setPassword(password);
-    return connectionFactory;
+    var factory = new CachingConnectionFactory("localhost");
+    factory.setUsername("guest");
+    factory.setPassword("guest");
+    return factory;
+  }
+
+  @Bean
+  public MessageConverter jsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public RabbitAdmin amqpAdmin() {
+    return new RabbitAdmin(connectionFactory());
   }
 
   @Bean
   public RabbitTemplate rabbitTemplate() {
-    return new RabbitTemplate(connectionFactory());
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+    rabbitTemplate.setMessageConverter(jsonMessageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public Queue colorsGetAllQueue() {
+    return new Queue("colors.getAll");
+  }
+
+  @Bean
+  public Queue colorsGetByIdQueue() {
+    return new Queue("colors.getById");
+  }
+
+  @Bean
+  public Queue colorsSaveQueue() {
+    return new Queue("colors.save");
+  }
+
+  @Bean
+  public Queue colorsDeleteQueue() {
+    return new Queue("colors.delete");
+  }
+
+  @Bean
+  public Queue colorsUpdateQueue() {
+    return new Queue("colors.update");
   }
 }
